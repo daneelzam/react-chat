fce73fd7cdeaa7524530fd6ec94783d595d469ec
Daniel Zameshaev <daneelzam@yandex.ru>
2021-01-18 10:49:04 +0300

    deploy of client part

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -24,7 +24,7 @@ function Chat({location}) {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://react-chat-dz.herokuapp.com/';
 
     useEffect(()=>{
         const {name, room} = queryString.parse(location.search);
